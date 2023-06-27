94d108cf75d01f21301c04328c004e64511e61f2
hash3liZer <sheikhshameerkashif@protonmail.com>
2023-06-27 18:21:09 +0500

    [Update] Update site configuration

--- src/lib/config/friends.ts
+++ src/lib/config/friends.ts
@@ -27,73 +27,73 @@ export type Friend = {
 }
 
 export const friends: Friend[] = [
-  {
-    id: 'mantyke',
-    rel: 'friend',
-    name: '塔塔',
-    title: '小球飞鱼',
-    link: 'https://mantyke.icu/',
-    descr: '我们会一起遇见鲸鱼吗？',
-    avatar: 'https://mantyke.icu/images/logo.png'
-  },
-  {
-    id: 'middleofnowhere',
-    rel: 'friend',
-    name: '陆博学',
-    title: 'Middle of No where',
-    link: 'https://notes.midofnowhere.link/',
-    descr: `Welcome to the middle of nowhere. That's right, absolute nowhere.`,
-    avatar: 'https://github.com/yue2/picbed/blob/main/cutepic.png?raw=true'
-  },
-  {
-    id: 'chestnut',
-    rel: 'friend',
-    name: '栗',
-    title: '野生栗子🌰',
-    link: 'https://blog.chestnut.monster/'
-  },
-  {
-    id: 'summerblue',
-    rel: 'friend',
-    name: '夏诤',
-    title: 'SummberBlue',
-    link: 'https://blog.summerblue.space/',
-    descr: '早睡早起身体好'
-  },
-  {
-    id: 'loikin',
-    rel: 'friend',
-    name: 'Loikin',
-    title: '此生未命名',
-    link: 'https://blog.loikein.one/',
-    descr: '用爱和理性对抗荒谬',
-    avatar: '/assets/loikin.png'
-  },
-  {
-    id: 'sharktale',
-    rel: 'friend',
-    name: '鲨',
-    title: '一只脆脆鲨',
-    link: 'https://woods.sharktale.xyz/',
-    descr: '遇见一只脆脆鲨',
-    avatar: 'https://s2.loli.net/2022/03/30/xwOzn9G8TIqFPvR.jpg'
-  },
-  {
-    id: 'kwaa',
-    rel: 'friend',
-    name: '藍+85CD',
-    title: './kwaa.dev',
-    link: 'https://kwaa.dev',
-    descr: '[DATA EXPUNGED]',
-    avatar: 'https://kwaa.dev/assets/any@512.webp'
-  },
-  {
-    id: 'debula',
-    rel: 'friend',
-    title: '秘密花园',
-    name: '本宝宝',
-    link: 'https://blog.debula.ml',
-    descr: '月亮一直跟着我走，它迷路了吗？',
-    avatar: 'https://blog.debula.ml/usr/uploads/violet.jpg'
-  }
+  // {
+  //   id: 'mantyke',
+  //   rel: 'friend',
+  //   name: '塔塔',
+  //   title: '小球飞鱼',
+  //   link: 'https://mantyke.icu/',
+  //   descr: '我们会一起遇见鲸鱼吗？',
+  //   avatar: 'https://mantyke.icu/images/logo.png'
+  // },
+  // {
+  //   id: 'middleofnowhere',
+  //   rel: 'friend',
+  //   name: '陆博学',
+  //   title: 'Middle of No where',
+  //   link: 'https://notes.midofnowhere.link/',
+  //   descr: `Welcome to the middle of nowhere. That's right, absolute nowhere.`,
+  //   avatar: 'https://github.com/yue2/picbed/blob/main/cutepic.png?raw=true'
+  // },
+  // {
+  //   id: 'chestnut',
+  //   rel: 'friend',
+  //   name: '栗',
+  //   title: '野生栗子🌰',
+  //   link: 'https://blog.chestnut.monster/'
+  // },
+  // {
+  //   id: 'summerblue',
+  //   rel: 'friend',
+  //   name: '夏诤',
+  //   title: 'SummberBlue',
+  //   link: 'https://blog.summerblue.space/',
+  //   descr: '早睡早起身体好'
+  // },
+  // {
+  //   id: 'loikin',
+  //   rel: 'friend',
+  //   name: 'Loikin',
+  //   title: '此生未命名',
+  //   link: 'https://blog.loikein.one/',
+  //   descr: '用爱和理性对抗荒谬',
+  //   avatar: '/assets/loikin.png'
+  // },
+  // {
+  //   id: 'sharktale',
+  //   rel: 'friend',
+  //   name: '鲨',
+  //   title: '一只脆脆鲨',
+  //   link: 'https://woods.sharktale.xyz/',
+  //   descr: '遇见一只脆脆鲨',
+  //   avatar: 'https://s2.loli.net/2022/03/30/xwOzn9G8TIqFPvR.jpg'
+  // },
+  // {
+  //   id: 'kwaa',
+  //   rel: 'friend',
+  //   name: '藍+85CD',
+  //   title: './kwaa.dev',
+  //   link: 'https://kwaa.dev',
+  //   descr: '[DATA EXPUNGED]',
+  //   avatar: 'https://kwaa.dev/assets/any@512.webp'
+  // },
+  // {
+  //   id: 'debula',
+  //   rel: 'friend',
+  //   title: '秘密花园',
+  //   name: '本宝宝',
+  //   link: 'https://blog.debula.ml',
+  //   descr: '月亮一直跟着我走，它迷路了吗？',
+  //   avatar: 'https://blog.debula.ml/usr/uploads/violet.jpg'
+  // }
 ]
