48a0a480ccb9ceaa1fba46927055a79c55ea0605
hash3liZer <hash3liZer@users.noreply.github.com>
2023-06-28 12:03:00 +0000

    deploy: 0f2f167e06b8eaeb08e189016fb615f786fbccd2

---
+++ _app/immutable/chunks/stores.5217046d.js
@@ -0,0 +1,1 @@
+import{s as e}from"./singletons.fa6d9692.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
